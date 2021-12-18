e0ebc069314ca001777a80007f49b2105fdbdf63
nira650 <niraxvi@gmail.com>
2021-12-18 21:57:47 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,300;0,400;0,500;0,700;0,900;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/icons/fa-icons/css/all.css">
+    <link rel="icon" href="assets/images/icon.png">
+    <title>Portfolio</title>
+</head>
+<body>
+    <header id="header">
+        <div class="row">
+            <div id="logo">
+                <img src="assets/images/logo.png" height="10%" width="10%" style="border-radius: 100px 100px 100px 100px;">
+
+            </div>
+            <div id="navigation">
+                <nav>
+                    <ul>
+                        <li>
+                            <a class="navigation-link" href="">Home</a>
+                        </li>
+                        <li>
+                            <a class="navigation-link" href="#about-me">About Me</a>
+                        </li>
+                        <li>
+                            <a class="navigation-link" href="#developments">Developments</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <div id="content">
+        <div class="row">
+            <div id="profile"> 
+                <div class="description">
+                    <div>Hi!</div>
+                    <div>I'm <span>Monira Libadia</span></div>
+                    <div>a BSIT student</div>
+                    <div>
+                        Web design, Front-end and Back-end Developer
+                    </div>
+                    <div class="social-media-profile">
+                        <ul>
+                            <a class="social-media-link" href="https://www.facebook.com/Moniraxlibadia">
+                                <li>
+                                    <i class="fab fa-facebook-f"></i>
+                                </li>
+                            </a>
+                            <a class="social-media-link" href="https://www.instagram.com/niraxvi">
+                                <li>
+                                    <i class="fab fa-instagram"></i>
+                                </li>
+                            </a>
+                            <a class="social-media-link" href="https://twitter.com/nira7077">
+                                <li>
+                                    <i class="fab fa-twitter"></i>
+                                </li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="image">
+                    <img src="assets/images/proo.jpg">
+                </div>
+            </div>
+            <div id="about-me">
+                <div class="section-title">ABOUT ME</div>
+                <div class="description"><p align="justify">
+                Hi, I'm Monira M. Libadia. My friends call me "Moneng". I have a strong passion for design and technology. I am currently twenty two year old and studying in Central Luzon State University taking up Bachelor of Science in Information Technology. I live in San Jose City, Nueva Ecija, Philippines. I love reading books, watching movies and kdrama, playing badminton with my cousins, playing online games and cooking in my spare time. I enjoy the simple things in life. I always look toward the next best thing. I'm a positive thinker person. I love seeing other people happy. </p>
+                </div>
+            </div>
+            <div id="skills">
+                <div class="section-title">SKILLS</div>
+                <div class="skills-wrapper">
+                    <div class="skill">
+                        <div class="image"><img src="assets/images/HTML.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">HTML</div>
+                            <div class="short-desc">
+                                HTML (HyperText Markup Language) is the most basic building block of the Web. It defines the meaning and structure of web content. With HTML you can create your own Website. 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="image"><img src="assets/images/css.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">CSS</div>
+                            <div class="short-desc">
+                                Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="image"><img src="assets/images/js.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">JavaScript</div>
+                            <div class="short-desc">
+                                JavaScript is a computer programming language. It is a client-side scripting language, which means the source code is processed by the client's web browser rather than on the web server. 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="image"><img src="assets/images/bootstrap.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Bootstrap</div>
+                            <div class="short-desc">
+                                Bootstrap programming, refers to the initial piece of code that is executed upon startup. The very first bit of code to run after startup is what the entire operating system depends on in order to work properly.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="image"><img src="assets/images/Java.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Java</div>
+                            <div class="short-desc">
+                                Java is an object-oriented programming language that produces software for multiple platforms. Java is most important in the current IT Sector, because Java is platform independent.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="image"><img src="assets/images/php.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">PHP</div>
+                            <div class="short-desc">
+                                PHP is an acronym for "PHP: Hypertext Preprocessor". PHP is a widely-used, open source scripting language. PHP scripts are executed on the server. 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="developments">
+            <div class="row">
+                <div class="section-title">DEVELOPMENTS</div>
+                <div class="developments-wrapper">
+                    <div class="development">
+                        <div class="image">
+                            <img src="assets/images/gnarly.png" alt="" srcset="">
+                        </div>
+                        <div class="description">
+                            <div class="title">Gnarly</div>
+                            <div class="short-desc">
+                                Gnarly! is a lifestyle brand dedicated to introducing and sustaining a streetwear culture. You can add to cart Skateboarding T-Shirts, Hoodies, Hats, and Accessories. The programming language used is Java.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="development">
+                        <div class="image">
+                            <img src="assets/images/landing.png" alt="" srcset="">
+                        </div>
+                        <div class="description">
+                            <div class="title">OSCA Online Registration</div>
+                            <div class="short-desc">
+                                An Online Registration – this system covers the creation and registration of membership based on the intended requirements needed by the OSCA. It has QR code for each new member of senior citizen. The programming language used is Laravel.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="development">
+                        <div class="image">
+                            <img src="assets/images/erms.png" alt="" srcset="">
+                        </div>
+                        <div class="description">
+                            <div class="title">Record Management System</div>
+                            <div class="short-desc">
+                                This system encompasses the development of a record management system addressing senior citizens of San Jose City as it will enhance OSCA’s processes and transaction handling. It handle personal information of all member of senior citizens of San Jose City, notify the members via SMS notification for reminders and upcoming events or announcements, monitor pension/allowance issued to senior citizens monthly whether the pension/allowance has been released already or not and provide reports for information per barangay monthly or annually if in case needed for assessment. And also have audit trail. The programming language used is Laravel.                            </div>
+                        </div>
+                    </div>
+                        <div class="development">
+                        <div class="image">
+                            <img src="assets/images/todoo.png" alt="" srcset="">
+                        </div>
+                        <div class="description">
+                            <div class="title">Student Planner</div>
+                            <div class="short-desc">
+                                Student Planner is an organizational tool that helps students stay on track with their work. It allows students to create and personalize even further with the option to create custom tasks and classes. The programming language used is Android Studio.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div id="hobbies">
+                <div class="section-title">HOBBIES</div>
+                <div class="hobbies-wrapper">
+                    <div class="hobby">
+                        <div class="image"><img src="assets/images/cod.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Coding</div>
+                            <div class="short-desc">
+                                One of the sure-fire ways to improve your coding skill is to practice and challenge yourself to do new things. It implies that you can improve your coding skill by always working on a programming project.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hobby">
+                        <div class="image"><img src="assets/images/watch.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Watching Movies/Series</div>
+                            <div class="short-desc">
+                                The best way to get rid of negative thoughts, and maybe find a solution to your problem, is watching movies. Movies are not just a way to have fun, but also a form of therapy. 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hobby">
+                        <div class="image"><img src="assets/images/Spotify.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Listening to music</div>
+                            <div class="short-desc">
+                                I listen to music when I get sad. Because scientists have found that the emotions patients experience while listening to music have a healthy effect on blood vessel function. Music both made study participants feel happier.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hobby">
+                        <div class="image"><img src="assets/images/book.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Reading Books</div>
+                            <div class="short-desc">
+                                The bookshelves are the standing source of joy to all book-lovers. To an educated man there is no pleasure comparable to the pleasures of reading books. Books provide us with varieties of entertainment.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hobby">
+                        <div class="image"><img src="assets/images/cooking.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Cooking</div>
+                            <div class="short-desc">
+                                With cooking, you can see (and taste!) your progress as your skills advance. It’s always nice when your efforts in a hobby pay off, but being able to actually see your progress can motivate you to stick with it. 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hobby">
+                        <div class="image"><img src="assets/images/badminton.png" alt="" srcset=""></div>
+                        <div class="description">
+                            <div class="title">Playing Badminton</div>
+                            <div class="short-desc">
+                                Badminton is an active sport in which the player’s entire body gets a workout. Badminton is a sport that many people enjoy playing and forming a bond over.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
+    <footer id="footer">
+        <div class="contacts">
+            <ul>
+                <a class="social-media-link" href="https://www.facebook.com/Moniraxlibadia">
+                    <li>
+                        <i class="fab fa-facebook-f"></i>
+                    </li>
+                </a>
+                <a class="social-media-link" href="https://www.instagram.com/niraxvi">
+                    <li>
+                        <i class="fab fa-instagram"></i>
+                    </li>
+                </a>
+                <a class="social-media-link" href="https://twitter.com/nira7077">
+                    <li>
+                        <i class="fab fa-twitter"></i>
+                    </li>
+                </a>
+                <a class="social-media-link" href="mailto: niraxvi@gmail.com">
+                    <li>
+                    <i class="fas fa-envelope"></i>
+                    </li>
+                </a>
+            </ul>
+        </div>
+        <div id="copyright">Copyright © 2021 <span>Monira Libadia</span>. All Rights Reserved.</div>
+    </footer>
+</body>
+</html>
